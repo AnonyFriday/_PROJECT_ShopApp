1a42668fb10501c061502aa44f8d371919207091
AnonyFriday <duykimvu75@gmail.com>
2023-12-21 00:50:50 +0700

    Added grid of products [Color on test mode]

---
+++ storeapp_frontend/src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  taskTitle = '';
+  isComplete = false;
+
+  updateTitle(newTitle: string) {
+    this.taskTitle = newTitle;
+  }
+
+  completeTask() {
+    this.isComplete = true;
+  }
+}

079313669106b0e94f77ab60007748ac28db1e00
AnonyFriday <duykimvu75@gmail.com>
2023-12-21 17:18:01 +0700

    Added the register page

--- storeapp_frontend/src/main.server.ts
+++ storeapp_frontend/src/main.server.ts
@@ -2,7 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { config } from './app/app.config.server';
 import { OrderComponent } from './app/order/order.component';
 import { LoginComponent } from './app/login/login.component';
+import { RegisterComponent } from './app/register/register.component';
 
-const bootstrap = () => bootstrapApplication(LoginComponent, config);
+const bootstrap = () => bootstrapApplication(RegisterComponent, config);
 
 export default bootstrap;
--- storeapp_frontend/src/main.ts
+++ storeapp_frontend/src/main.ts
@@ -2,7 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { OrderComponent } from './app/order/order.component';
 import { LoginComponent } from './app/login/login.component';
+import { RegisterComponent } from './app/register/register.component';
 
-bootstrapApplication(LoginComponent, appConfig).catch((err) =>
+bootstrapApplication(RegisterComponent, appConfig).catch((err) =>
   console.error(err)
 );
